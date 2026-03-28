9746d1eeb9388401ed55d805198ef2a225b1cd71
Luc Alexander <lucskyalex@gmail.com>
2026-03-28 21:25:05 +0100

    apend eval loop

--- src/main.zig
+++ src/main.zig
@@ -49,6 +49,7 @@ pub fn tokenize(mem: *const std.mem.Allocator, text: []const u8) Buffer(Token) {
 				outer: while (k < text.len){
 					const kc = text[k];
 					switch (kc){
+						' ', '\n', '\t',
 						OPEN_TERM,
 						CLOSE_TERM,
 						IMPL_TERM,
@@ -79,17 +80,57 @@ pub fn tokenize(mem: *const std.mem.Allocator, text: []const u8) Buffer(Token) {
 
 const Instruction = struct {
 	name: ?Token,
-	comp: ?Program
+	comp: ?Program,
+
+	pub fn eql(a: Instruction, b: Instruction) bool {
+		if (a.name) |n| {
+			if (b.name) |m| {
+				if (n.tag != m.tag){
+					return false;
+				}
+				if (std.mem.eql(u8, n.text, m.text)){
+					return true;
+				}
+			}
+			else {
+				return false;
+			}
+		}
+		else if (a.comp) |c| {
+			if (b.comp) |k| {
+				if (c.data.items.len != k.data.items.len){
+					return false;
+				}
+				for (c.data.items, k.data.items) |left, right| {
+					if (Instruction.eql(left, right) == false){
+						return false;
+					}
+				}
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+		return false;
+	}
+};
+
+const Arg = union(enum) {
+	named: Instruction,
+	atom: Instruction
 };
 
 const Term = struct {
-	fptr: []Instruction,
-	trigger: []Instruction,
-	result: []Instruction
+	bound_start: u64,
+	bound_end: u64,
+	args: Buffer(Arg),
+	body: []Instruction
 };
 
 const Program = struct {
-	terms: Buffer(Term),
+	mem: *const std.mem.Allocator,
+	tmp: *const std.mem.Allocator,
 	data: Buffer(Instruction),
 
 	pub fn show(self: *const Program) void {
@@ -120,7 +161,8 @@ const ParseError = error {
 
 pub fn parse(mem: *const std.mem.Allocator, tokens: []const Token, i: *u64) ParseError!Program {
 	var program = Program{
-		.terms = Buffer(Term).init(mem.*),
+		.mem = mem,
+		.tmp = mem,
 		.data = Buffer(Instruction).init(mem.*)
 	};
 	while (i.* < tokens.len){
@@ -172,15 +214,166 @@ pub fn parse(mem: *const std.mem.Allocator, tokens: []const Token, i: *u64) Pars
 	return program;
 }
 
+pub fn parse_term(mem: *const std.mem.Allocator, instructions: []Instruction, i: *u64) ?Term {
+	const save = i.*;
+	i.* += 1;
+	var term = Term{
+		.bound_start = 0,
+		.bound_end = 0,
+		.args = Buffer(Arg).init(mem.*),
+		.body = instructions
+	};
+	while (i.* < instructions.len){
+		const inst = instructions[i.*];
+		if (inst.name) |n| {
+			if (n.tag == IMPL_TERM){
+				break;
+			}
+			if (n.tag == ARG_TERM){
+				i.* += 1;
+				if (i.* == instructions.len){
+					i.* = save;
+					return null;
+				}
+				term.args.append(Arg{.named = instructions[i.*]}) catch unreachable;
+			}
+			else{
+				term.args.append(Arg{.atom = instructions[i.*]}) catch unreachable;
+			}
+		}
+		i.* += 1;
+	}
+	i.* += 1;
+	const body = i.*;
+	while (i.* < instructions.len){
+		const inst = instructions[i.*];
+		if (inst.name) |n|{
+			if (n.tag == CLOSE_TERM){
+				break;
+			}
+		}
+		i.* += 1;
+	}
+	term.body = instructions[body .. i.*];
+	term.bound_start = save;
+	term.bound_end = i.*;
+	i.* += 1;
+	return term;
+}
+
+pub fn apply(program: *Program, i: u64, term: Term) bool {
+	var k: u64 = i;
+	var arg: u64 = 0;
+	var argmap = Map(Instruction).init(program.tmp.*);
+	if (term.args.items.len == 0){
+		return false;
+	}
+	while (arg < term.args.items.len){
+		if (k == program.data.items.len){
+			return false;
+		}
+		if (k >= term.bound_start and k < term.bound_end){
+			return false;
+		}
+		if (term.args.items[arg] == .named){
+			if (term.args.items[arg].named.name) |name|{
+				if (argmap.get(name.text)) |inst|{
+					if (Instruction.eql(inst, program.data.items[k]) == false){
+						return false;
+					}
+				}
+				else{
+					argmap.put(name.text, program.data.items[k]) catch unreachable;
+				}
+			}
+			else{
+				return false;
+			}
+		}
+		else{
+			if (Instruction.eql(program.data.items[k], term.args.items[arg].atom) == false){
+				return false;
+			}
+		}
+		k += 1;
+		arg += 1;
+	}
+	var data = Buffer(Instruction).init(program.mem.*);
+	data.appendSlice(program.data.items[0..i]) catch unreachable;
+	for (term.body) |atom| {
+		if (atom.name) |name| {
+			if (argmap.get(name.text)) |alias| {
+				data.append(alias) catch unreachable;
+				continue;
+			}
+		}
+		data.append(atom) catch unreachable;
+	}
+	data.appendSlice(program.data.items[k..program.data.items.len]) catch unreachable;
+	program.data = data;
+	return true;
+}
+
+pub fn eval_step(program: *Program) bool {
+	var i: u64 = 0;
+	var terms = Buffer(Term).init(program.mem.*);
+	while (i < program.data.items.len) {
+		const inst = program.data.items[i];
+		if (inst.name) |n| {
+			if (n.tag == OPEN_TERM){
+				if (parse_term(program.mem, program.data.items, &i)) |term| {
+					terms.append(term) catch unreachable;
+				}
+			}
+		}
+		i += 1;
+	}
+	i = 0;
+	while (i < program.data.items.len){
+		var k: u64 = terms.items.len;
+		while (k > 0){
+			const term = terms.items[k-1];
+			if (apply(program, i, term)){
+				return true;
+			}
+			k -= 1;
+		}
+		i += 1;
+	}
+	return false;
+}
+
+pub fn append_step(program: *Program, stream: *Program) bool {
+	if (stream.data.items.len == 0){
+		return false;
+	}
+	program.data.append(stream.data.items[0]) catch unreachable;
+	_ = stream.data.orderedRemove(0);
+	program.show();
+	std.debug.print("\n", .{});
+	while (eval_step(program)){
+		std.debug.print(" => ", .{});
+		program.show();
+		std.debug.print("\n", .{});
+	}
+	return true;
+}
+
 pub fn main() !void {
 	const heap = std.heap.page_allocator;
 	const main_buffer = heap.alloc(u8, 0x100000000) catch unreachable;
 	var main_mem_fixed = std.heap.FixedBufferAllocator.init(main_buffer);
 	var main_mem = main_mem_fixed.allocator();
-	const program_text = "[.x .y swp: y x] 1/(a b c) (2 3) swp";
+	const program_text = "[.x .y swp: y x]";
+	const stream_text = "1 2 swp";
 	const tokens = tokenize(&main_mem, program_text);
+	const input_tokens = tokenize(&main_mem, stream_text);
 	var i: u64 = 0;
-	const program = parse(&main_mem, tokens.items, &i) catch unreachable;
-	program.show();
-	std.debug.print("\n", .{});
+	var program = parse(&main_mem, tokens.items, &i) catch unreachable;
+	i = 0;
+	var stream = parse(&main_mem, input_tokens.items, &i) catch unreachable;
+	while (append_step(&program, &stream)){
+		program.show();
+		std.debug.print("\n", .{});
+	}
 }
